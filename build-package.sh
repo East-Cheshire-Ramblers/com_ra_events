f39cbfa778fad4fb07528e37baaba6578e1b0834
Richard Higham <richard.higham@hahosting.com>
2026-06-26 10:06:44 +0100

    Add RA events module package

--- build-package.sh
+++ build-package.sh
@@ -5,6 +5,10 @@ component="com_ra_events"
 manifest="ra_events.xml"
 version="$(sed -n 's:.*<version>\(.*\)</version>.*:\1:p' "$manifest" | head -1)"
 package="${component}-${version}.zip"
+module="mod_ra_events"
+module_manifest="modules/${module}/${module}.xml"
+module_version="$(sed -n 's:.*<version>\(.*\)</version>.*:\1:p' "$module_manifest" | head -1)"
+module_package="${module}-${module_version}.zip"
 
 rm -rf dist
 mkdir -p dist
@@ -12,7 +16,22 @@ mkdir -p dist
 zip -r "dist/${package}" "$manifest" admin forms src tmpl \
 	-x '.DS_Store' \
 	-x '*/.DS_Store' \
+	-x '._*' \
+	-x '*/._*' \
 	-x '*.old' \
 	-x '*.old2'
 
 echo "Created dist/${package}"
+
+(
+	cd "modules/${module}"
+	zip -r "../../dist/${module_package}" "${module}.xml" index.html "${module}.php" tmpl \
+		-x '.DS_Store' \
+		-x '*/.DS_Store' \
+		-x '._*' \
+		-x '*/._*' \
+		-x '*.old' \
+		-x '*.old2'
+)
+
+echo "Created dist/${module_package}"
